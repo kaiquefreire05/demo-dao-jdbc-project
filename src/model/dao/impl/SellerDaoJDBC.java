9a7837ce10cd0cd25dcad43cd392e31a47c205e1
Kaíque Freire <kaiquefreiresantos05@gmail.com>
2023-11-27 18:50:43 -0300

    reusing instantiantion

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -55,18 +55,8 @@ public class SellerDaoJDBC implements SellerDao {
 
 			if (rs.next()) {
 				// instância o departamento
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId")); // colocando o valor no objeto através da table
-				dep.setName(rs.getString("DepName")); // colocando o valor no objeto através da table
-				
-				Seller obj = new Seller(); // criando o objeto seller
-				// colocando os valores da tabela
-				obj.setId(rs.getInt("id"));
-				obj.setName(rs.getString("Name"));
-				obj.setEmail(rs.getString("Email"));
-				obj.setBaseSalary(rs.getDouble("BaseSalary"));
-				obj.setBirthDate(rs.getDate("BirthDate"));
-				obj.setDepartment(dep); // fazendo uma junção de objeto, ele já está declarado no código então não precisa usar get
+				Department dep = instantiateDepartment(rs); 
+				Seller obj = instantiateSeller(rs, dep);
 				return obj; // retornando o objeto completo
 			}
 			
@@ -80,6 +70,28 @@ public class SellerDaoJDBC implements SellerDao {
 			DB.closeResultSet(rs);
 		}
 		
+	}
+	// método auxiliar para instanciar seller
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		// TODO Auto-generated method stub
+		Seller obj = new Seller(); // criando o objeto seller
+		// colocando os valores da tabela
+		obj.setId(rs.getInt("id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep); // fazendo uma junção de objeto, ele já está declarado no código então não precisa usar get
+		return obj;
+	}
+
+	// método auxiliar para instanciar departament
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		// TODO Auto-generated method stub
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId")); // colocando o valor no objeto através da table
+		dep.setName(rs.getString("DepName")); // colocando o valor no objeto através da table
+		return dep;
 	}
 
 	@Override
