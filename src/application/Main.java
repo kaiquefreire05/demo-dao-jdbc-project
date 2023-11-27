f5d6fce409d614f9bb1ec599df9450a80345c4e3
Kaíque Freire <kaiquefreiresantos05@gmail.com>
2023-11-27 15:04:11 -0300

    seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Main {
 
@@ -8,7 +11,10 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Kaique", "kaique@gamil.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 	}
 
 }
